5211fb57e52abff2675002291e69ef9aa6a077ef
Jehanzaib <jehanzaib.dev@gmail.com>
2026-04-21 12:27:38 +0500

    Add: cor library for render

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import dotenv from 'dotenv';
 import connectMongoDB from './src/config/database.js';
+import cors from "cors";
 import app from './src/app.js';
 
 dotenv.config({
     path:'./.env'
 });
+
+    app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 const startServer=async()=>{
 
